02de2dbf8e8d5e978e603c27e6201d23e80ec0d2
Infeso64 <infeso64@gmail.com>
2024-06-02 19:37:28 +0200

    Inital

---
+++ src/main/java/net/infeso/betteradventuremode/BetterAdventureModeClient.java
@@ -0,0 +1,10 @@
+package net.infeso.betteradventuremode;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterAdventureModeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
