b8d98db41a16a7edf227ddcf12b5f2571dab53ed
Infeso64 <infeso64@gmail.com>
2024-06-02 20:33:51 +0200

    Items Added

--- src/main/java/net/infeso/betteradventuremode/BetterAdventureMode.java
+++ src/main/java/net/infeso/betteradventuremode/BetterAdventureMode.java
@@ -2,6 +2,8 @@ package net.infeso.betteradventuremode;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.infeso.betteradventuremode.item.ModItemGroups;
+import net.infeso.betteradventuremode.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,7 +12,9 @@ public class BetterAdventureMode implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
-	public void onInitialize() {
-
+	public void onInitialize()
+	{
+		ModItems.registerModItem();
+		ModItemGroups.registerItemGroups();
 	}
 }
